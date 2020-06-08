09e5ddc8d0c951d0e004faeba1e6e99e57f4f761
Hervé Beraud <hberaud@redhat.com>
2020-06-08 23:02:26 +0200

    Use unittest.mock instead of mock
    The mock third party library was needed for mock support in py2
    runtimes. Since we now only support py36 and later, we can use the
    standard lib unittest.mock module instead.
    
    Note that https://github.com/openstack/charms.openstack is used during tests
    and he need `mock`, unfortunatelly it doesn't declare `mock` in its
    requirements so it retrieve mock from other charm project (cross dependency).
    So we depend on charms.openstack first and when
    Ib1ed5b598a52375e29e247db9ab4786df5b6d142 will be merged then CI
    will pass without errors.
    
    Depends-On: Ib1ed5b598a52375e29e247db9ab4786df5b6d142
    Change-Id: I62e4e03f73db6e4ddd10b8d221a2ae8fda0a4290

--- unit_tests/test_reactive_neutron_api_plugin_ovn_handlers.py
+++ unit_tests/test_reactive_neutron_api_plugin_ovn_handlers.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import mock
+from unittest import mock
 
 import reactive.neutron_api_plugin_ovn_handlers as handlers
 
